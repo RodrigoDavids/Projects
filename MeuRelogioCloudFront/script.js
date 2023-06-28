822bf438607123faf113223fe96c21a7a4e6e171
RodrigoDavids <rodrigo.davids@hotmail.com.br>
2023-06-27 22:56:29 -0300

    Code review

---
+++ MeuRelogioCloudFront/script.js
@@ -0,0 +1,30 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MeuRelogio").innerText = time;
+  document.getElementById("MeuRelogio").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
