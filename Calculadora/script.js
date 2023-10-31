c6c7449a0300f0bd613f4de9903f9a1a4c604c3d
RodrigoDavids <rodrigo.davids@hotmail.com.br>
2023-10-31 01:04:20 -0300

    commit

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,4 +1,15 @@
-var nome = prompt("Qual e o seu nome")
+
+function displaynum(n1){
+Calculator.text1.value = Calculator.text1.value + n1;
+}
+
+
+
+
+
+
+
+/*var nome = prompt("Qual e o seu nome")
 var idade = prompt("Sua Idade");
 if (idade>= 18 ){
     alert('Voce é de maior');
@@ -6,8 +17,11 @@ if (idade>= 18 ){
     alert("Voce é de menor "+ nome)
 }
 
-//digoalert(" "+ nome"tem" + idade)
 
+
+
+//digoalert(" "+ nome"tem" + idade)
+/*
 var signo = prompt("Qual é o seu signo?");
 
 if (signo.toLowerCase() == "Capricornio") {
@@ -19,3 +33,5 @@ if (signo.toLowerCase() == "Capricornio") {
 //var sign = prompt(); //  abrir uma janela de promtpt em branco
 var sign = window.prompt("Você está se sentindo com sorte"); // abrir uma janela com o texto "Você está se sentindo com sorte"
 var sign = window.prompt("Você está se sentindo com sorte", "certamente"); // abrir uma janela com o texto "Você está se sentindo com sorte" e com o valor padrão "certamente"
+*/
+
