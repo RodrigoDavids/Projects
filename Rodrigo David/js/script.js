c8e032e8121f0feebe75698c217743cad711867c
RodrigoDavids <rodrigo.davids@hotmail.com.br>
2023-12-14 03:37:26 -0300

    codando

--- Rodrigo David/js/script.js
+++ Rodrigo David/js/script.js
@@ -1,23 +1,3 @@
-$(function () {
+window.sr = ScrollReveal ({ reset:true});
 
-
-    //HEADER
-    $(window).scroll(function () {
-          if($(this).scrollTop() > 200)
-          {
-              if (!$('.main_header').hasClass('fixed'))
-              {
-                  $('.main_header').stop().addClass('fixed').css('top', '-100px').animate(
-                      {
-                          'top': '0px'
-                      }, 500);
-              }
-          }
-          else
-          {
-              $('.main_header').removeClass('fixed');
-          }
-    });
-
-
-});
+sr.reveal('')
